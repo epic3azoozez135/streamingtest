a4447d92322e5b39892928319cb562745730a7bf
Azoooooooooz <44898336+epic3azoozez135@users.noreply.github.com>
2018-12-23 12:01:13 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,61 +1,34 @@
-const Discord = require("discord.js")
-const client = new Discord.Client();
-var prefix = "-a"
+const devs = ["462038057529507841"];
+const adminprefix = ["-a"];
+client.on('message', message => {
+    var argresult = message.content.split(` `).slice(1).join(' ');
+      if (!devs.includes(message.author.id)) return;
 
-
-
-client.on('message', function(message) {
-    const myID = "511491524077420555";
-    let args = message.content.split(" ").slice(1).join(" ");
-    if(message.content.startsWith(prefix + "sn")) {
-                if(message.author.id !== myID) return;
-            if(!args) return message.reply('???? ?????? ???? ??????.');
-        client.user.setUsername(args);
-        message.channel.send('**SetName,Done!**').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "s")) {
-                if(message.author.id !== myID) return;
-            if(!args) return message.reply('???? ?????? ???? ??????.');
-        client.user.setGame(args , 'https://twitch.tv/6xlez1');
-        message.channel.send('**Streaming,Done!??**').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "p")) {
-                        if(message.author.id !== myID) return;
-            if(!args) return message.reply('???? ?????? ???? ??????.');
-        client.user.setGame(args);
-        message.channel.send('**Playing,Done!??**').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "l")) {
-                        if(message.author.id !== myID) return;
-            if(!args) return message.reply('???? ?????? ???? ??????.');
-        client.user.setActivity(args, {type:'LISTENING'});
-        message.channel.send('**Listening,Done!??**').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "w")) {
-                        if(message.author.id !== myID) return;
-            if(!args) return message.reply('???? ?????? ???? ??????.');
-        client.user.setActivity(args, {type:'WATCHING'});
-        message.channel.send('**Watching,Done!??**').then(msg => {
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    } else if(message.content.startsWith(prefix + "setavatar")) {
-                        if(message.author.id !== myID) return;
-        client.user.setAvatar(args);
-        message.channel.send(':white_check_mark: Done!').then(msg => {
-                if(!args) return message.reply('???? ?????? ???? ??????.');
-           msg.delete(5000);
-          message.delete(5000);
-        });
-    }
+  if (message.content.startsWith(adminprefix + 'ply')) {
+    client.user.setGame(argresult);
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+     if (message.content === (adminprefix + "leave")) {
+    message.guild.leave();
+  } else
+  if (message.content.startsWith(adminprefix + 'wt')) {
+  client.user.setActivity(argresult, {type:'WATCHING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'ls')) {
+  client.user.setActivity(argresult , {type:'LISTENING'});
+      message.channel.send(`**✅   ${argresult}**`)
+  } else
+  if (message.content.startsWith(adminprefix + 'st')) {
+    client.user.setGame(argresult, "https://www.twitch.tv/idk");
+      message.channel.send(`**✅**`)
+  }
+  if (message.content.startsWith(adminprefix + 'setname')) {
+  client.user.setUsername(argresult).then
+      message.channel.send(`Changing The Name To ..**${argresult}** `)
+} else
+if (message.content.startsWith(adminprefix + 'setavatar')) {
+  client.user.setAvatar(argresult);
+    message.channel.send(`Changing The Avatar To :**${argresult}** `);
+}
 });
-
-client.login(process.env.BOT_TOKEN); 
